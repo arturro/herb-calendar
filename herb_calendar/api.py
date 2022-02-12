6c80a50d987746240d75c4a122d2cb5f6120a55d
Artur Kurek <artur.kurek@gmail.com>
2022-02-12 18:52:05 +0100

    plants rest

--- herb_calendar/api.py
+++ herb_calendar/api.py
@@ -2,4 +2,4 @@ from rest_framework import routers
 from plants import views as plants_views
 
 router = routers.DefaultRouter()
-router.register(r'plant', plants_views.PlantViewSetCategoryViewSet)
+router.register(r'plant', plants_views.PlantViewSet)
